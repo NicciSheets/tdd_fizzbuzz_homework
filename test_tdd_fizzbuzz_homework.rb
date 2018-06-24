7be3212f764e08cc7744ef5f8a542434457768bb
Nicci Sheets <niccisheets@aol.com>
2018-06-24 18:21:33 -0400

    failed test for every 3rd position replaced with Mined

--- test_tdd_fizzbuzz_homework.rb
+++ test_tdd_fizzbuzz_homework.rb
@@ -11,5 +11,8 @@ class TestTddFizzbuzzHomework < Minitest::Test
 		assert_equal(100, fizzbuzz().length)
 	end
 
+	def test_assert_that_every_3rd_position_is_mined
+		assert_equal("Mined", fizzbuzz()[2])
+	end
 
 end
