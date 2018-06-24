620eca08802fabcc0f5cb08f40ea23a00429d7ab
Nicci Sheets <niccisheets@aol.com>
2018-06-24 19:12:47 -0400

    passed test for every 15th element is mined minds

--- tdd_fizzbuzz_homework.rb
+++ tdd_fizzbuzz_homework.rb
@@ -2,7 +2,9 @@ def fizzbuzz()
 	empty = []
 	arr = [*1..100]
 	arr.each do |n|
-		if n % 3 == 0
+		if n % 15 == 0
+			n = "Mined Minds"
+		elsif n % 3 == 0
 			n = "Mined"
 		elsif n % 5 == 0
 			n = "Minds"
